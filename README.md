73448c56f61d308aeb4f0f3d74d9e19d7a6f9940
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-08-24 19:13:20 +0900

    Initial version

---
+++ README.md
@@ -0,0 +1,36 @@
+# ddu-source-vim
+
+Vim script source for ddu.vim
+
+This source collects items from Vim script functions.
+
+## Required
+
+### denops.vim
+
+https://github.com/vim-denops/denops.vim
+
+### ddu.vim
+
+https://github.com/Shougo/ddu.vim
+
+## Configuration
+
+```vim
+" Use neomru.vim as ddu source
+" https://github.com/Shougo/neomru.vim
+function! NeoMruSource()
+  return map(neomru#_gather_file_candidates(), { _, path -> {
+        \   'word': path,
+        \   'kind': 'file',
+        \   'action': {
+        \      'path': path,
+        \      'isDirectory': isdirectory(path),
+        \   },
+        \ }})
+endfunction
+
+call ddu#start({'sources': [
+\   {'name': 'vim', 'params': {'func': 'NeoMruSource'}},
+\ ]})
+```
---
+++ denops/@ddu-sources/vim.ts
@@ -0,0 +1,37 @@
+import {
+  BaseSource,
+  Context,
+  Item,
+} from "https://deno.land/x/ddu_vim@v1.9.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v1.9.0/deps.ts";
+
+type Params = {
+  func: string;
+};
+
+export type ActionData = Record<never, never>;
+
+export class Source extends BaseSource<Params> {
+  gather(args: {
+    denops: Denops;
+    context: Context;
+    sourceParams: Params;
+  }): ReadableStream<Item<ActionData>[]> {
+    return new ReadableStream({
+      async start(controller) {
+        if (args.sourceParams.func != "") {
+          const items = await args.denops.call(
+            args.sourceParams.func) as Item<ActionData>[]
+          controller.enqueue(items);
+        }
+        controller.close();
+      },
+    });
+  }
+
+  params(): Params {
+    return {
+      func: "",
+    };
+  }
+}
