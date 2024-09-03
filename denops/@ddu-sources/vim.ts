80fd6adc1314f98f186ee43dbbe2c8064df7cfa1
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-09-03 19:23:02 +0900

    Update deps

--- denops/@ddu-sources/vim.ts
+++ denops/@ddu-sources/vim.ts
@@ -1,8 +1,6 @@
-import {
-  BaseSource,
-  type Context,
-  type Item,
-} from "jsr:@shougo/ddu-vim@^5.0.0/types";
+import { type Context, type Item } from "jsr:@shougo/ddu-vim@~6.1.0/types";
+import { BaseSource } from "jsr:@shougo/ddu-vim@~6.1.0/source";
+
 import type { Denops } from "jsr:@denops/core@^7.0.0";
 
 type Params = {
@@ -21,7 +19,8 @@ export class Source extends BaseSource<Params> {
       async start(controller) {
         if (args.sourceParams.func != "") {
           const items = await args.denops.call(
-            args.sourceParams.func) as Item<ActionData>[]
+            args.sourceParams.func,
+          ) as Item<ActionData>[];
           controller.enqueue(items);
         }
         controller.close();
